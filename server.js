bb05d491a22f0f9abab615054ab657a23778e074
ali <ali@alis-MacBook-Pro.local>
2026-05-23 08:13:26 +0500

    Fix CORS policy for zordpakistan.shop

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
-  'https://zord-pakistan-paypal1.vercel.app'
+  'https://zord-pakistan-paypal1.vercel.app',
+  'https://zordpakistan.shop'
 ];
 app.use(cors({
   origin: (origin, callback) => {
